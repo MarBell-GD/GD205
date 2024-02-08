bed74e09f5c6e7c260b916e659be4ad731d09fb6
Marlon <geometrydashmarbell@gmail.com>
2024-02-08 18:27:09 -0500

    initial commit

---
+++ Assets/Scripts/1 - grid/GridPlayer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         if (Input.GetKeyDown(KeyCode.W))
+         {
+
+            transform.position += Vector3.back;
+
+         }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            transform.position += Vector3.forward;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            transform.position += Vector3.right;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            transform.position += Vector3.left;
+
+        }
+
+    }
+}
