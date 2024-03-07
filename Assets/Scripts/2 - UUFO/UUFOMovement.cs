d20b6732686599f4f6682e410e5100598f51a4c1
Marlon <geometrydashmarbell@gmail.com>
2024-03-07 17:17:46 -0500

    school version

---
+++ Assets/Scripts/2 - UUFO/UUFOMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UUFOMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float spd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+
+            rb.AddForce(Vector3.forward * spd * Input.GetAxisRaw("Vertical"));
+
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
+        {
+
+            rb.AddForce(Vector3.right * spd * Input.GetAxisRaw("Horizontal"));
+
+        }
+
+    }
+
+}
