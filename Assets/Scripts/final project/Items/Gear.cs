1a58822a4bfd5d47afdf65db67311c29e36b3a67
Marlon <geometrydashmarbell@gmail.com>
2024-04-26 04:37:23 -0400

    lil change

--- Assets/Scripts/final project/Items/Gear.cs
+++ Assets/Scripts/final project/Items/Gear.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class Gear : ScriptableObject
 {
-    
 
+    [HideInInspector] public int ID;
+
+    public string Name;
+    [TextArea(3, 5)] public string Description;
 
 }
